e9e2be4a3517c1213acafc1373425ed88d384b6d
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-11-20 17:04:57 -0500

    add some routes

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 import express from "express";
 
+const posts = [];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is ready...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Welcome !!");
+app.get("/posts", (req, res) => {
+    res.status(200).send("Return All Posts");
+});
+
+function getPostById(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    }) 
+}
+
+app.get("/posts/:id", (req, res) => {
+    const i = getPostById(req.params.id)
+    res.status(200).send(posts[i]);
 });
